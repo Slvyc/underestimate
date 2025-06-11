35cd71bb1fa2f2f29fe9cadd810d12443c77f1b5
Slvyc <daffaalfharizy265@gmail.com>
2025-06-11 14:21:27 +0700

    roadmap

--- resources/views/profile/kalenderAkademik.blade.php
+++ resources/views/profile/kalenderAkademik.blade.php
@@ -8,79 +8,79 @@
 </section>
 
 <!-- Struktur Organisasi Section -->
-<section class="kalender-akademik-section">
-    @foreach ( $KalenderAkademiks as $KalenderAkademik )
-    {{-- <div class="container-kalender-akademik text-center">
-        <!-- Gambar Struktur Organisasi -->
-        <img src="{{ asset('storage/' . $KalenderAkademik->image_ganjil) }}" alt="Kalender Akademik Ganjil" class="org-image-kalender">
-        <!-- Download Button -->
-        <div class="mt-3">
-            <a href="{{ asset('storage/' . $KalenderAkademik->image_ganjil) }}" download class="btn btn-custom">
-                <i class="bi bi-download"></i> Download 
-            </a>`
+    <section class="kalender-akademik-section">
+        @foreach ( $KalenderAkademiks as $KalenderAkademik )
+        {{-- <div class="container-kalender-akademik text-center">
+            <!-- Gambar Struktur Organisasi -->
+            <img src="{{ asset('storage/' . $KalenderAkademik->image_ganjil) }}" alt="Kalender Akademik Ganjil" class="org-image-kalender">
+            <!-- Download Button -->
+            <div class="mt-3">
+                <a href="{{ asset('storage/' . $KalenderAkademik->image_ganjil) }}" download class="btn btn-custom">
+                    <i class="bi bi-download"></i> Download 
+                </a>`
+            </div>
+        </div> --}}
+
+        <div class="container-kalender-akademik text-center">
+            <!-- Gambar Struktur Organisasi -->
+            <img src="{{ asset('storage/' . $KalenderAkademik->image_genap) }}" alt="Kalender Akademik Genap" class="org-image-kalender">
+            <!-- Download Button -->
+            <div class="mt-3">
+                <a href="{{ asset('storage/' . $KalenderAkademik->image_genap) }}" download class="btn btn-custom">
+                    <i class="bi bi-download"></i> Download 
+                </a>
+            </div>
         </div>
-    </div> --}}
-
-    <div class="container-kalender-akademik text-center">
-        <!-- Gambar Struktur Organisasi -->
-        <img src="{{ asset('storage/' . $KalenderAkademik->image_genap) }}" alt="Kalender Akademik Genap" class="org-image-kalender">
-        <!-- Download Button -->
-        <div class="mt-3">
-            <a href="{{ asset('storage/' . $KalenderAkademik->image_genap) }}" download class="btn btn-custom">
-                <i class="bi bi-download"></i> Download 
-            </a>
-        </div>
-    </div>
-    @endforeach
-
-    <div class="roster-container">
-        <h2>Roster Akademik</h2>
-        @foreach ( $RosterAkademiks as $RosterAkademik)
-            <div class="roster-card">
-                <h3>Teknik Sipil</h3>
-                <iframe 
-                    src="{{ asset('pdfjs/web/viewer.html?file=' . asset('storage/' . $RosterAkademik->roster_sipil)) }}" 
-                    width="100%" 
-                    height="600px" 
-                    style="border: none;">
-                </iframe>
-                <div class="mt-2 text-center">
-                    <a href="{{ asset('storage/' . $RosterAkademik->roster_sipil) }}" download class="btn btn-custom">
-                        <i class="bi bi-download"></i> Download 
-                    </a>
-                </div>
-            </div>
-
-            <div class="roster-card">
-                <h3>Teknik Mesin</h3>
-                <iframe 
-                    src="{{ asset('pdfjs/web/viewer.html?file=' . asset('storage/' . $RosterAkademik->roster_mesin)) }}" 
-                    width="100%" 
-                    height="600px" 
-                    style="border: none;">
-                </iframe>
-                <div class="mt-2 text-center">
-                    <a href="{{ asset('storage/' . $RosterAkademik->roster_mesin) }}" download class="btn btn-custom">
-                        <i class="bi bi-download"></i> Download 
-                    </a>
-                </div>
-            </div>
-
-            <div class="roster-card">
-                <h3>Sistem Informasi</h3>
-                <iframe 
-                    src="{{ asset('pdfjs/web/viewer.html?file=' . asset('storage/' . $RosterAkademik->roster_sistem_informasi)) }}" 
-                    width="100%" 
-                    height="600px" 
-                    style="border: none;">
-                </iframe>
-                <div class="mt-2 text-center">
-                    <a href="{{ asset('storage/' . $RosterAkademik->roster_sistem_informasi) }}" download class="btn btn-custom">
-                        <i class="bi bi-download"></i> Download 
-                    </a>
-                </div>
-            </div>
         @endforeach
-    </div>
-</section>
+
+        <div class="roster-container">
+            <h2>Roster Akademik</h2>
+            @foreach ( $RosterAkademiks as $RosterAkademik)
+                <div class="roster-card">
+                    <h3>Teknik Sipil</h3>
+                    <iframe 
+                        src="{{ asset('pdfjs/web/viewer.html?file=' . asset('storage/' . $RosterAkademik->roster_sipil)) }}" 
+                        width="100%" 
+                        height="600px" 
+                        style="border: none;">
+                    </iframe>
+                    <div class="mt-2 text-center">
+                        <a href="{{ asset('storage/' . $RosterAkademik->roster_sipil) }}" download class="btn btn-custom">
+                            <i class="bi bi-download"></i> Download 
+                        </a>
+                    </div>
+                </div>
+
+                <div class="roster-card">
+                    <h3>Teknik Mesin</h3>
+                    <iframe 
+                        src="{{ asset('pdfjs/web/viewer.html?file=' . asset('storage/' . $RosterAkademik->roster_mesin)) }}" 
+                        width="100%" 
+                        height="600px" 
+                        style="border: none;">
+                    </iframe>
+                    <div class="mt-2 text-center">
+                        <a href="{{ asset('storage/' . $RosterAkademik->roster_mesin) }}" download class="btn btn-custom">
+                            <i class="bi bi-download"></i> Download 
+                        </a>
+                    </div>
+                </div>
+
+                <div class="roster-card">
+                    <h3>Sistem Informasi</h3>
+                    <iframe 
+                        src="{{ asset('pdfjs/web/viewer.html?file=' . asset('storage/' . $RosterAkademik->roster_sistem_informasi)) }}" 
+                        width="100%" 
+                        height="600px" 
+                        style="border: none;">
+                    </iframe>
+                    <div class="mt-2 text-center">
+                        <a href="{{ asset('storage/' . $RosterAkademik->roster_sistem_informasi) }}" download class="btn btn-custom">
+                            <i class="bi bi-download"></i> Download 
+                        </a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </section>
 @endsection
--- resources/views/profile/roadmap.blade.php
+++ resources/views/profile/roadmap.blade.php
@@ -10,12 +10,30 @@
 </section>
 
 <!-- Roadmap Section -->
-{{-- <section class="struktur-section">
+<section class="struktur-section">
     <div class="container-organisasi text-center">
         <!-- Gambar Roadmap -->
-        <img src="asset\img\Struktur Fakultas Teknik.png" class="org-image">
+        <img src="asset\img\Roadmap Fakultas Teknik.jpeg" class="org-image">
         <!-- Ganti URL gambar di atas dengan path gambar struktur organisasi Anda -->
     </div>
-</section> --}}
+</section>
+<section class="kalender-akademik-section">
+    <div class="roster-container">
+        <h2>Roadmap</h2>
+        <div class="roster-card">
+            <iframe 
+                src="pdfjs/web/viewer.html?file=\asset\Roadmap-FT-UNAYA- 2022-2026.pdf" 
+                width="100%" 
+                height="600px" 
+                style="border: none;">
+            </iframe>
+            <div class="mt-2 text-center">
+                <a href="asset/Roadmap-FT-UNAYA- 2022-2026.pdf" download class="btn btn-custom">
+                    <i class="bi bi-download"></i> Download 
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
 
